ce41c5b920849766e538236030afdaf40dca0716
kkwik <kevinkwik@proton.me>
2025-10-29 11:11:14 -0700

    chore: update build script for multiple c files

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 
 rm main 2> /dev/null # Ignore error message
-gcc -o main main.c -Wall -Wextra -Werror -Wpedantic -lpthread -lm -lncurses
+gcc -o main *.c -Wall -Wextra -Werror -Wpedantic -lpthread -lm -lncurses
